7b5f59d5cc4923c518b9bf5f18b8a462b64ab9a4
Hong <keepthemonochrome@gmail.com>
2017-03-29 12:21:17 -0700

    Add resource

--- addFunction.js
+++ addFunction.js
@@ -13,3 +13,6 @@ function add(a,b){
 
 console.log(add(2,5));
 console.log(add(2)(5));
+
+//Study more about functional programming
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0#.u8awq68sg
