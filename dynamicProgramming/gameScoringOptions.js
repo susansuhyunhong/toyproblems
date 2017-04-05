a4a587b72751d445a06dd3a3ec1bce996d14cb8f
Hong <keepthemonochrome@gmail.com>
2017-04-05 12:00:25 -0700

    Add new dp problem discription

---
+++ dynamicProgramming/gameScoringOptions.js
@@ -0,0 +1,8 @@
+// Imagine a game (like baseball) where a player can score 1,2 or 4 runs.
+// Given a score "n", find the total number of ways score "n" can be reached.
+//
+// To score 3 runs a player can score the following 3 ways.
+// n = 3
+// 1, 1, 1
+// 1, 2
+// 2, 1
