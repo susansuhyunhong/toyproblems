2eb16017424d0b8cc3a9f4978bf3900947d94f58
Hong <keepthemonochrome@gmail.com>
2017-04-25 00:43:55 -0700

    Add whiteboarding problem

---
+++ wordBreak.js
@@ -0,0 +1,3 @@
+// Given a dictionary of words and an input string tell whether the input string can be completely segmented into dictionary words.
+// Sol1 - recursion
+// Sol2 - make dictionary into Trie
